7b4583fd971b3e6ff8a506e83ff13a656f208f00
Luxur <tony8000a@gmail.com>
2015-11-13 16:15:56 +0800

    fixed output

--- output.py
+++ output.py
@@ -1,1 +1,15 @@
-aaa
+# -*- coding: utf-8 -*- 
+import json
+
+def get_result(longitude, latitude):
+	result = {
+		'bus_no': '藍7', 
+		'predict_time': 12, 
+		'predict_cost': 45, 
+		'attractions': ['維元家', '新崛江']
+	}
+
+	return json.dumps(result, sort_keys= True, encoding = 'utf8', ensure_ascii= False)
+
+
+print get_result(123.22, 23.8)
