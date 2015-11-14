905cab52006f81c70dfe3ffd70fb9f95006ad9f4
Luxur <tony8000a@gmail.com>
2015-11-14 17:01:23 +0800

    replace tab with 4 space

--- output.py
+++ output.py
@@ -2,14 +2,14 @@
 import json
 
 def get_result(longitude, latitude):
-	result = {
-		'bus_no': '藍7', 
-		'predict_time': 12, 
-		'predict_cost': 45, 
-		'attractions': ['維元家', '新崛江']
-	}
+    result = {
+        'bus_no': '藍7', 
+        'expected_time': 12, 
+        'expected_cost': 45, 
+        'attractions': ['維元家', '新崛江']
+    }
 
-	return json.dumps(result, sort_keys= True, encoding = 'utf8', ensure_ascii= False)
+    return json.dumps(result, sort_keys= True, encoding = 'utf8', ensure_ascii= False)
 
 
 print get_result(123.22, 23.8)
