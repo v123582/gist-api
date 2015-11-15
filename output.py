68c86405f763ea3ed39f3467c1702efcaa703dad
Luxur <tony8000a@gmail.com>
2015-11-15 16:18:01 +0800

    load Bus.json & MRT.json into two variables

--- output.py
+++ output.py
@@ -14,8 +14,6 @@ def get_result(longitude, latitude):
     with open('./datasrc/MRT.json') as data_file:
         mrt_info = json.load(data_file)
     
-
     return json.dumps(result, sort_keys= True, encoding = 'utf8', ensure_ascii= False)
-
 
 print get_result(123.22, 23.8)
