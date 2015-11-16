82dceabb7e1a61888a251ad2f71d6f175d6c56cd
v123582 <v123582@gmail.com>
2015-11-17 03:50:00 +0800

    update file structue: static & template

--- api.py
+++ api.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*- 
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.route('/show')
+def show():
+    return render_template('show.html')
 
 class index(Resource):
     def get(self):
